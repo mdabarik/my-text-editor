dea798b4d5015306825b747a83cbe1d31f5d1b7e
mdabarik <mdabarik19@gmail.com>
2023-08-17 04:36:49 +0600

    simplified

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 
 // add event listener on bold
 document.getElementById('bold').addEventListener('click', function() {
-    addClassById('text-field', 'font-extrabold');
+    const textField = document.getElementById('text-field');
+    textField.classList.toggle("font-bold");
 });
 // add event listener on italic
 document.getElementById('italic').addEventListener('click', function() {
@@ -17,51 +18,30 @@ document.getElementById('underline').addEventListener('click', function() {
 // add event listener on left
 document.getElementById('left').addEventListener('click', function() {
     const textField = document.getElementById('text-field');
-    if (textField.classList.contains('text-right')) textField.classList.remove('text-right');
-    if (textField.classList.contains('text-center')) textField.classList.remove('text-center');
-    if (!textField.classList.contains("text-left"))
-        textField.classList.add("text-left");
-    else
-        textField.classList.remove("text-left");
+    textField.style.textAlign = 'left';
 });
 // add event listener on center
 document.getElementById('center').addEventListener('click', function() {
     const textField = document.getElementById('text-field');
-    if (textField.classList.contains('text-left')) textField.classList.remove('text-left');
-    if (textField.classList.contains('text-right')) textField.classList.remove('text-right');
-    if (!textField.classList.contains("text-center"))
-        textField.classList.add("text-center");
-    else
-        textField.classList.remove("text-center");
+    textField.style.textAlign = 'center';
 });
 // add even listener on right
 document.getElementById('right').addEventListener('click', function() {
     const textField = document.getElementById('text-field');
-    if (textField.classList.contains('text-left')) textField.classList.remove('text-left');
-    if (textField.classList.contains('text-center')) textField.classList.remove('text-center');
-    if (!textField.classList.contains("text-right"))
-        textField.classList.add("text-right");
-    else
-        textField.classList.remove("text-right");
+    textField.style.textAlign = 'right';
 });
 
 document.getElementById('font-size').addEventListener('blur', function() {
     let getFontSizeElement = document.getElementById('font-size');
     let getFontSizeValue = parseInt(getFontSizeElement.value);
     getFontSizeElement = 0;
-    console.log(getFontSizeValue);
     const textField = document.getElementById('text-field');
-    textField.classList.add(`text-[${getFontSizeValue}px]`);
+    textField.style.fontSize = getFontSizeValue + 'px';
 });
 
 document.getElementById('color').addEventListener('blur', function() {
     let colorString = document.getElementById('color').value;
     const textField = document.getElementById('text-field');
-    for (let i = textField.classList.length - 1; i >= 0; i--) {
-        const className = textField.classList[i];
-        if (className.startsWith('text-')) {
-          textField.classList.remove(className);
-        }
-      }
-    textField.classList.add(`text-[${colorString}]`);
+    // textField.style.color = colorString;
+    textField.setAttribute('style', `color: ${colorString}`);
 })
